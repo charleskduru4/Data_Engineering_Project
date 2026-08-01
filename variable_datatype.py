1644d690d42492db3c316f7808f403aba5d05f5c
CHARLES-KENNEDY <charleskennedyduru4@gmail.com>
2026-08-01 15:04:15 -0500

    Create variable_datatype.py

---
+++ variable_datatype.py
@@ -0,0 +1,21 @@
+# Variable and Data type
+
+# first_name : str = "John Charles"
+# height : int = None
+
+# print(first_name, height)
+
+"""creating python Variable: A pyhon variable is created automatically 
+when you assign a value to it. The equal(=) is used to assign 
+values to variable.
+"""   
+counter : int = 100
+miles : float = 1000.0
+name: str = "Roli"
+    
+# To print the variables value
+# print(counter, miles, name)
+
+print(miles)
+print(name)
+print(counter)
