f23d9770302b6870f9aa9abff9df1d8a09f4970e
CHARLES-KENNEDY <charleskennedyduru4@gmail.com>
2026-08-01 23:39:45 -0500

    Update variable_datatype.py

--- variable_datatype.py
+++ variable_datatype.py
@@ -16,21 +16,43 @@ values to variable or a variable is a reserved memory area
 Creating python Variable: We can assign a value to the variable
 we can use the assignment operator = to assign a value to a variable 
 """ 
-counter : int = 100
-miles : float = 1000.0
-name: str = "Peter"
+# counter : int = 100
+# miles : float = 1000.0
+# name: str = "Peter"
 
-# To print the variables value
-# print(counter, miles, name)
+# # To print the variables value
+# # print(counter, miles, name)
 
-print(miles)
-print(name)
-print(counter)
+# print(miles)
+# print(name)
+# print(counter)
 
 """
-Changing the value of a variable: Python variable are dynamicall
+Changing the value of a variable: Python variable are dynamically
 typed and not subject to the data type restriction.
 """
+var : int = 10
+print(var)
+# print its type
+print(type(var))
+
+# Assign different integer value to var
+var: int = 55
+print(var)
+
+# Change var to string
+var: str = "This is example of a string"
+print(var)
+
+# print its type
+print(type(var))
+
+#Change var to float
+var:float = 35.69
+print(var)
+
+# print its type
+print(type(var))
     
     
     
