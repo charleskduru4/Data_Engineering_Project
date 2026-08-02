be666c639b216e1b9a15329f27395db48bfb14b2
CHARLES-KENNEDY <charleskennedyduru4@gmail.com>
2026-08-01 20:57:29 -0500

    Update variable_datatype.py

--- variable_datatype.py
+++ variable_datatype.py
@@ -5,13 +5,17 @@
 
 # print(first_name, height)
 
-"""creating python Variable: A pyhon variable is created automatically 
+"""A python variable is created automatically 
 when you assign a value to it. The equal(=) is used to assign 
-values to variable.
-"""   
+values to variable or a variable is a reserved memory area 
+(memory address to store value)
+""" 
+
+# Creating python Variable: We can assign a value to the variable
+# we can use the assignment operator = to assign a value to a variable  
 counter : int = 100
 miles : float = 1000.0
-name: str = "Roli"
+name: str = "Peter"
     
 # To print the variables value
 # print(counter, miles, name)
@@ -19,3 +23,8 @@ name: str = "Roli"
 print(miles)
 print(name)
 print(counter)
+
+    """Changing the value of a variable: Python variable are dynamicall typed
+    and not subject to the data type restriction. A variable may be assigned to 
+    a value of one type, and then we can re-assigned a value of a different type
+    """
